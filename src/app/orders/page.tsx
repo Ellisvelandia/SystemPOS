f3447d0da0b5dc48bb6a741d9fa40dfceee9e25a
Ellis velandia <eyis619@gmail.com>
2024-11-18 11:32:28 -0500

    app

--- src/app/orders/page.tsx
+++ src/app/orders/page.tsx
@@ -1,15 +1,19 @@
 import { DataTable } from "@/components/data-table";
 import { columns } from "./columns";
 import { orders } from "@/lib/data";
+import { Order } from "./columns";
 
 export default function OrdersPage() {
+  // Create a mutable copy of the orders array
+  const mutableOrders: Order[] = [...orders];
+
   return (
     <div className="h-full flex flex-col">
       <div className="flex items-center justify-between p-6 border-b">
         <h1 className="text-2xl font-bold">Orders</h1>
       </div>
       <div className="p-6 flex-1">
-        <DataTable columns={columns} data={orders} />
+        <DataTable columns={columns} data={mutableOrders} />
       </div>
     </div>
   );
